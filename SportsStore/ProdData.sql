902798be8e53cabf11672e8b073d2edbd8f01c67
kiranpdas <30925350+kiranpdas@users.noreply.github.com>
2018-03-02 10:16:39 -0500

    Initial commit

---
+++ SportsStore/ProdData.sql
@@ -0,0 +1,8 @@
+﻿insert into Products values('Lifejacket',' Protective and fashionable ','Watersports ',48.95);
+insert into Products values('Soccer',' Ball FIFA-approved size and weight ','Soccer ',19.5);
+insert into Products values('Corner',' Flags Give your playing field a professional touch ','Soccer ',34.95);
+insert into Products values('Stadium',' Flat-packed, 35,000-seat stadium ','Soccer ',79500);
+insert into Products values('Thinking Cap ','Improve your brain efficiency by 75% ','Chess ',16);
+insert into Products values('Unsteady Chair ','Secretly give your opponent a disadvantage ','Chess ',29.95);
+insert into Products values('Human Chess Board ','A fun game for the family ','Chess ',75);
+insert into Products values('Bling-Bling King',' Gold-plated, diamond-studded King ','Chess ',1200);
---
+++ SportsStore/SportsStore.WebUI/Controllers/NavController.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Mvc;
+namespace SportsStore.WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+        public string Menu()
+        {
+            return "Hello from NavController";
+        }
+    }
+}
